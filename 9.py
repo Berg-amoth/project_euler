c3547a35a4131dbc31f7f262c1373c669ae23b49
Berg-amoth <alexis.japas@protonmail.com>
2020-05-11 19:31:06 +0200

    Problem n°9 achieved : c and b initiated at their maximum while a at minimum, testing each combination

---
+++ 9.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 11 00:37:03 2020
+
+@author: amoth
+"""
+
+import math as m
+
+# a < b < c < x/2 < a+b
+def pythagorean_triplet(x):
+    # c and b starting at their maximum while a initiate at minimum
+    c = m.floor(x/2)
+    b = c - 1
+    a = x - c - b
+    while a**2 + b**2 != c**2:
+        if b <= a:
+            c -= 1
+            b = c - 1
+            a = x - c - b
+        else:
+            b -= 1
+            a += 1
+    return a*b*c
+
+
+print(pythagorean_triplet(1000))
